38144db6699cb9d9702f935b90391c0432a0d489
Mateusz Dobrzanski <mateuszdobrzasnki@outlook.com>
2019-05-11 23:54:34 +0200

    added controller and views

--- src/main/java/com/demo/UploadApplication.java
+++ src/main/java/com/demo/UploadApplication.java
@@ -1,13 +1,39 @@
 package com.demo;
 
+import org.apache.coyote.http11.AbstractHttp11Protocol;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatConnectorCustomizer;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class UploadApplication {
 
+	private int maxUploadSizeInMb = 10 * 1024 * 1024; // 10 MB
+
 	public static void main(String[] args) {
+
 		SpringApplication.run(UploadApplication.class, args);
+	}
+
+	//Tomcat large file upload connection reset
+	//http://www.mkyong.com/spring/spring-file-upload-and-connection-reset-issue/
+	@Bean
+	public ServletWebServerFactory tomcatEmbedded() {
+
+		TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+
+		tomcat.addConnectorCustomizers((TomcatConnectorCustomizer) connector -> {
+			if ((connector.getProtocolHandler() instanceof AbstractHttp11Protocol<?>)) {
+				//-1 means unlimited
+				((AbstractHttp11Protocol<?>) connector.getProtocolHandler()).setMaxSwallowSize(-1);
+			}
+		});
+
+		return tomcat;
+
 	}
 
 }
